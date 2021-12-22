76f04cc120a04aa84e4c8c3f1610d8e4245d0f26
Mikhail Kalnitskiy <interpetersburg@yandex.ru>
2021-12-22 10:16:45 +0100

    module-2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,25 @@
-function sumSalary(salaries) {
-  // ваш код...
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+
+function sumSalary(salaries) {
+  let sum = 0;
+  for (let key in salaries) {
+
+    if (typeof salaries[key] == 'number' && isFinite(salaries[key])) {
+      sum += salaries[key];
+    } else {
+      sum = 0;
+    }
+  }
+  return sum;
+}
+
+
+console.log(sumSalary(salaries));
