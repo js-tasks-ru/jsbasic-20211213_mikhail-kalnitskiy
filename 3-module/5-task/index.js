2012449a6d84f07400a0ab32f91fd333fc58713c
Mikhail Kalnitskiy <interpetersburg@yandex.ru>
2021-12-25 17:14:48 +0100

    git commit -am clear

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,20 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = {};
+  let arr = str.split(' ');
+  let arrNumbers = [];
+
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == 'Number') {
+      arrNumbers.push(+arr[i])
+    }
+  }
+
+
+  arrNumbers.sort();
+  result.min = arrNumbers[0];
+  result.max = arrNumbers[arrNumbers.length];
+
+  return result;
+
 }
